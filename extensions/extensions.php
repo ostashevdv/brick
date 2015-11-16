e463a35d946d71fe259922c7e29b864edb90b61b
ostashev <ostashev@shogo.ru>
2015-11-16 17:26:09 +0300

    архитектура конфигурации

--- extensions/extensions.php
+++ extensions/extensions.php
@@ -1,3 +1,38 @@
 <?php
 
-return [];
+use yii\helpers\ArrayHelper;
+
+Yii::setAlias('@ext', dirname(__FILE__));
+
+$extensionsDir = __DIR__;
+
+/**
+ * $extensions = [
+ *  'vendor/name' => [
+ *      'name' => 'vendor/name',
+ *      'version' => '9999999-dev',
+ *      'alias' => [
+ *          '@vendor/name' => $extensionsDir . '/vendor/directory',
+ *      ],
+ *  ],
+ * ];
+ */
+$extensions = [
+    'brick/wpo' => [
+        'name' => 'brick/wpo',
+        'version' => '9999999-dev',
+        'alias' => [
+            '@brick/wpo' => $extensionsDir . '/brick-wpo',
+        ],
+    ],
+];
+
+foreach ($extensions as $extension) {
+    if (isset($extension['alias'])) {
+        foreach ($extension['alias'] as $alias => $path) {
+            Yii::setAlias($alias, $path);
+        }
+    }
+}
+
+return ArrayHelper::merge($extensions, require_once dirname(__DIR__) . '/vendor/yiisoft/extensions.php');
--- web/index.php
+++ web/index.php
@@ -1,13 +1,22 @@
 <?php
 
-//Удалите эти строки на production
+//Включение DEBUG режима
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
 
 require(__DIR__ . '/../vendor/autoload.php');
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
-$config = require(dirname(__DIR__) . '/app/config/app.php');
+$config = ['app' => [], 'local' => []];
+
+$local = dirname(__DIR__) . '/app/config/local.php';
+if (file_exists($local)) {
+    $config['local'] = require($local);
+}
+
+$config['app'] = require(dirname(__DIR__) . '/app/config/app.php');
+
+$config = \yii\helpers\ArrayHelper::merge($config['app'], $config['local']);
 
 $app = new \yii\web\Application($config);
 require(__DIR__ . '/../app/config/bootstrap.php');
